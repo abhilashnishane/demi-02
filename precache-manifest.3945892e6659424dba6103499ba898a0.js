8c670089aa76444b62d3c353a912f7b14e1380c0
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-09-21 23:52:39 +0530

    Updates

---
+++ precache-manifest.3945892e6659424dba6103499ba898a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed0672e3588bbea50effcc4e4ebfe51",
+    "url": "/demi-02/index.html"
+  },
+  {
+    "revision": "b726d027070c9cda3af4",
+    "url": "/demi-02/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7013c0ea6ef1abc19bdd",
+    "url": "/demi-02/static/css/main.03ec14d1.chunk.css"
+  },
+  {
+    "revision": "b726d027070c9cda3af4",
+    "url": "/demi-02/static/js/2.d3f7a7a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demi-02/static/js/2.d3f7a7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7013c0ea6ef1abc19bdd",
+    "url": "/demi-02/static/js/main.3ef9ff82.chunk.js"
+  },
+  {
+    "revision": "6ad464fd2a2d0703babf",
+    "url": "/demi-02/static/js/runtime-main.f7fe747a.js"
+  }
+]);
